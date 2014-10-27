e9526d2dd5694a3b84c31f6c098cf00b9d8b934a
qianfeng <qianfeng@qianfengdeMac-mini058.local>
2014-10-27 10:19:24 +0800

    Initial Commit

---
+++ FlappyBird-Formal/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  FlappyBird-Formal
+//
+//  Created by qianfeng on 14-10-27.
+//  Copyright (c) 2014年 jyw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
